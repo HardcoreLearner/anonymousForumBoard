08bdd92a3566d7a6add66ba270ce8665dbb79cf8
HardcoreLearner <MoBah@protonmail.com>
2024-03-30 13:57:55 +0000

    trying the new message form implementation

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,16 @@
+const Message = require("../models/message");
+
 exports.home_get = (req, res) => {
     res.render("home", { user: req.user});
 }
+
+exports.new_message = async (req, res) => {
+    const message = req.body.message;
+    console.log(message);
+    const newMessage = new Message({
+        author: user.id,
+        message: message
+    });
+    await newMessage.save();
+    res.redirect("/");
+};
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 const homeController = require("../controllers/homeController");
 router.get("/", homeController.home_get);
+router.post("/", homeController.new_message)
 
 module.exports = router;
